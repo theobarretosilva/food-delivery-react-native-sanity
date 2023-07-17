e52b309c1287cd6feec7fbc08592e69487d4e6f0
Théo Barreto Silva <barretotheo25@gmail.com>
2023-07-17 19:40:10 -0300

    upload

--- schemas/index.js
+++ schemas/index.js
@@ -1,6 +1,6 @@
-import blockContent from './blockContent'
 import category from './category'
-import post from './post'
-import author from './author'
+import dish from './dish'
+import featured from './featured'
+import restaurant from './restaurant'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [category, dish, featured, restaurant]
